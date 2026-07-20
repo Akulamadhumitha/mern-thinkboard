e6c24fcb03cb9cd5d7d53d16ef084165d26c9983
Akulamadhumitha <akulamadhumitha11@gmail.com>
2026-07-20 16:19:52 +0530

    done upto updateNote controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,12 +1,38 @@
-export function getAllNotes(req,res){
-    res.status(200).send("you fetched the notes");
+import Note from "../models/Note.js";
+
+export async function getAllNotes(req,res){
+    try{
+       const notes = await Note.find();
+       res.status(200).json(notes);
+    }
+    catch(error){
+       console.error("error in getAllNotes controller",error);
+       res.status(500).json({message:"internal server error"});
+    }
 }
-export function createNote(req,res){
-    res.status(201).json({message:"note created successfully"});
+export async function createNote(req,res){
+    try{
+        const {title,content} = req.body;
+        const note = new Note({title,content});
+        const savedNote = await note.save();
+        res.status(201).json(savedNote);
+    }
+    catch(error){
+        console.error("error in createNote controller",error);
+        res.status(500).json({message:"internal server error"});
+    }
 }
-export function updateNote(req,res){
-    res.status(200).json({message:"note updated successfully"});
+export async function updateNote(req,res){
+    try{
+        const {title,content} = req.body;
+        await Note.findByIdAndUpdate(req.params.id,{title,content});
+        res.status(200).json({message:"note updated successfully"});
+    }
+    catch(error){
+        console.error("error in createNote controller",error);
+        res.status(500).json({message:"internal server error"});
+    }
 }
-export function deleteNote(req,res){
+export async function deleteNote(req,res){
     res.status(200).json({message:"note deleted successfully"});
 }
