d34e083fb9a87d170aa13a5e1f4634bbf019b9a3
Akulamadhumitha <akulamadhumitha11@gmail.com>
2026-07-21 15:53:39 +0530

    done upto get specific note controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -10,6 +10,19 @@ export async function getAllNotes(req,res){
        res.status(500).json({message:"internal server error"});
     }
 }
+export async function getNoteById(req,res) {
+    try{
+        const getNote = await Note.findById(req.params.id);
+        if(!getNote){
+            return res.status(404).json({message:"note not found"});
+        }
+        res.status(200).json(getNote);
+    }
+    catch(error){
+       console.error("error in getOneNote controller",error);
+       res.status(500).json({message:"internal server error"});
+    }
+}
 export async function createNote(req,res){
     try{
         const {title,content} = req.body;
@@ -25,8 +38,13 @@ export async function createNote(req,res){
 export async function updateNote(req,res){
     try{
         const {title,content} = req.body;
-        await Note.findByIdAndUpdate(req.params.id,{title,content});
-        res.status(200).json({message:"note updated successfully"});
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id,{title,content},{
+            new:true
+        });
+        if(!updatedNote){
+            return res.status(404).json({message:"note not found"})
+        }
+        res.status(200).json(updatedNote);
     }
     catch(error){
         console.error("error in createNote controller",error);
@@ -34,5 +52,15 @@ export async function updateNote(req,res){
     }
 }
 export async function deleteNote(req,res){
-    res.status(200).json({message:"note deleted successfully"});
+    try{
+       const deletedNote = await Note.findByIdAndDelete(req.params.id);
+       if(!deletedNote){
+        return res.status(404).json({message:"no note to delete"});
+       }
+       res.status(200).json(deletedNote);
+    }
+    catch(error){
+        console.error("error in deleteNote controller",error);
+        res.status(500).json({message:"internal server error"});
+    }
 }
